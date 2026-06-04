7a31097e1d1c3590de76f8dcb41f373a84c7ef87
vnau <20983143+vnau@users.noreply.github.com>
2026-06-05 00:33:27 +0200

    upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const importMetaUrlPolyfill = '__import_meta_url__';
+
 export default defineConfig({
+	build: {
+		rolldownOptions: {
+			transform: {
+				define: {
+					'import.meta.url': importMetaUrlPolyfill
+				}
+			},
+			output: {
+				intro:
+					"var _documentCurrentScript = typeof document !== 'undefined' ? document.currentScript : null;" +
+					`var ${importMetaUrlPolyfill} = (_documentCurrentScript && _documentCurrentScript.tagName.toUpperCase() === 'SCRIPT' && _documentCurrentScript.src || (typeof document !== 'undefined' ? document.baseURI : typeof location !== 'undefined' ? location.href : ''));`
+			}
+		}
+	},
 	plugins: [sveltekit()]
 });
